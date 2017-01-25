ce0da8e0c7ac9a5d50a503de9626d6217df2bf15
IanBraddock <ianbraddock77@gmail.com>
2017-01-24 23:45:18 -0800

    Add files via upload
    more play around

---
+++ chapter10.py
@@ -0,0 +1,67 @@
+'''
+def letsAdd(x,y):
+    addition = x + y
+    return addition
+
+print letsAdd(33,785)
+
+
+def addit(x,y):
+    z = x + y
+    return z
+
+print addit(45,67)
+'''
+someValue = 5
+def letsAdd(x,y):
+    addition = x + y
+    return addition
+
+print letsAdd(3,5)
+print someValue
+
+def subtraction(x, y):
+    subtract = x - y
+    return subtract
+
+print subtraction(4, 5)
+
+def moreSubtraction (x, y, z):
+    subtract = x - y - z
+    return subtract
+
+print moreSubtraction (4, 7, 45) 
+
+def mult (x, y):
+    mult = x * y
+    return mult
+
+print mult(4, 78)
+
+def divide(x, y):
+    divi = float(x) / float(y)
+    return divi
+
+print divide(98, 34)
+
+length = len("How epic are built-in functions, huh?")
+print length
+
+x = 23
+print str(x)
+
+x = 2.32
+print str(x)
+
+y = float(40)/float(7)
+print y
+
+yint = int(y)
+print yint
+
+y = float(45)/float(34)
+print y
+
+print round(y)
+
+print int(round(y))
