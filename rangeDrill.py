74e03ab20395b38591bdbba3bb48dcc6a773a011
IanBraddock <ianbraddock77@gmail.com>
2017-01-24 23:43:39 -0800

    Add files via upload
    drill using the range function in python

---
+++ rangeDrill.py
@@ -0,0 +1,10 @@
+listy = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in range(4):
+    print(listy[i])  
+
+for i in range(3, -1, -1):
+    print (listy[i])
+
+for i in range(8, 0, -2):
+    print (listy[i])
